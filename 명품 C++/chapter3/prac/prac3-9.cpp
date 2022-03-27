a1c8bfd333f06ea7a950671c7dd94f5272e02173
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-03-27 18:28:45 +0900

    Update prac3-9.cpp

--- 명품 C++/chapter3/prac/prac3-9.cpp
+++ 명품 C++/chapter3/prac/prac3-9.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 using namespace std;
 
-// Oval Ŭ���� �����
+// Oval 클래스 선언부
 class Oval {
 	int width, height;
 public:
-	//������
+	//생성자
 	Oval();
 	Oval(int w, int h);
-	//�Ҹ���
+	//소멸자
 	~Oval(); 
-	//Ÿ�� �ʺ� ���� �Լ�
+	//타원 너비 리턴 함수
 	int getWidth();
-	//Ÿ������ ���� �Լ�
+	//타원높이 리턴 함수
 	int getHeight();
-	//Ÿ�� �ʺ�,���� ���� �Լ�
+	//타원 너비,높이 변경 함수
 	void set(int w, int h);
-	//�ʺ�,���� ��� �Լ�
+	//너비,높이 출력 함수
 	void show();
 };
 
-// Oval Ŭ���� ������
-//������
-Oval::Oval() { // �� �����ڴ� �� �ٷ� �ۼ� ����. Oval::Oval() : Oval(1) { }
+// Oval 클래스 구현부
+//생성자
+//위임생성자로 작성 가능하지만 이 부분은 따로 구현해 주었음
+Oval::Oval() { // 이 생성자는 한 줄로 작성 가능. Oval::Oval() : Oval(1) { }
 	width = height = 1;
 }
 Oval::Oval(int w, int h) {
 	width = w; height = h;
 }
-//�Ҹ��� ������
+//소멸자 구현부
 Oval::~Oval() {
-	cout << "Oval �Ҹ� : ";
+	cout << "Oval 소멸 : ";
 	show();
 }
 void Oval::set(int w, int h) {
@@ -46,7 +47,7 @@ int Oval::getHeight() {
 	return height;
 }
 
-// main() �Լ�
+// main() 함수
 int main() {
 	Oval a, b(3, 4);
 	a.set(10, 20);
