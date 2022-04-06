e1d1586d3b7f599411f14c850e9dd6c63f4b125f
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-04-06 20:20:24 +0900

    Create 1-1.cpp

---
+++ 명품 C++/chapter1/1-1.cpp
@@ -0,0 +1,11 @@
+/* 비주얼 스튜디오를 활용하여 다음에 지시한 대로 C++ 프로그램을
+* 작성하고 실행하라.
+*/
+
+#include <iostream>
+
+int main() {
+	//using namespace std를 하지 않아서 cout을 사용할때 std::를 해주어야 함.
+	std::cout << "My name is Mike.\n";
+	return 0;
+}
