9aa38999d36c55a14af52fb9dc8ac3c01f4551c6
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-04-06 20:38:18 +0900

    1-2.cpp

---
+++ 명품 C++/chapter1/1-2.cpp
@@ -0,0 +1,14 @@
+/*
+* 학과, 나이, 희망직업을 화면에 출력하는 프로그램을 작성하라
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "컴퓨터공학과" << endl;
+	cout << "23세" << endl;
+	cout << "개발자" << endl;
+
+	return 0;
+}
