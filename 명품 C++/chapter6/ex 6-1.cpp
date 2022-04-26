418d2cc97cd8c0a3bc786b637e2de0c96ed36827
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-04-26 21:24:29 +0900

    encoding solve

--- 명품 C++/chapter6/ex 6-1.cpp
+++ 명품 C++/chapter6/ex 6-1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 using namespace std;
 
 int big(int a, int b) {
@@ -19,6 +19,6 @@ int big(int a[], int size) {
 
 int main() {
 	int array[5] = { 1,9,-2,8,6 };
-	cout << big(2, 3) << endl;	//int big(int a, int b) È£Ãâ
-	cout << big(array, 5) << endl;	//int big(int a[], int size) È£Ãâ
+	cout << big(2, 3) << endl;	//int big(int a, int b) í˜¸ì¶œ
+	cout << big(array, 5) << endl;	//int big(int a[], int size) í˜¸ì¶œ
 }
