0b88eb487e1a7aa10eb2485791a95ae33e114b47
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-05-04 11:22:48 +0900

    example chapter7

---
+++ 명품 C++/chapter7/example ch7/example 7-1.cpp
@@ -0,0 +1,36 @@
+/* 2개의 Rect 객체를 비교하는 bool equals(Rect r, Rect s)를 Rect class의 friend 함수로 작성하라*/ 
+
+#include <iostream>
+using namespace std;
+
+
+class Rect; //Rect class가 선언되기 전에 먼저 참조되는 컴파일 오류(forward reference)를 막기위한 선언문(forward declaration))
+bool equals(Rect r, Rect s); //equals() 함수 선언
+
+
+//Rect class 선언
+class Rect {
+	int width, height;
+public:
+	Rect(int width, int height) { this->width = width; this->height = height; }	//인라인 함수로 선언및 구현함
+	friend bool equals(Rect r, Rect s); //equals() 함수를 프렌드 함수로 선언
+
+};
+
+
+//equals()함수 구현, equals()함수는 외부 함수임
+bool equals(Rect r, Rect s) {
+	if (r.width == s.width && r.height == s.height)	//외부 함수이지만 Rect class의 friend 함수로 선언되어서 private한 width,height 변수에 접근 가능
+		return true;
+	else
+		return false;
+}
+
+
+int main() {
+	Rect a(3, 4), b(4, 5);
+	if (equals(a, b))
+		cout << "equal" << endl;
+	else
+		cout << "not equal" << endl;
+}
---
+++ 명품 C++/chapter7/example ch7/example 7-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Rect; //Rect class가 선언되기 전에 먼저 참조되는 컴파일 오류(forward regerence)를 막기위한 선언문(forward declaration)
+
+//RectManager class 선언
+class RectManager {
+public:
+	bool equals(Rect r, Rect s);
+};
+
+//Rect class 선언
+class Rect {
+	int width, height;
+public:
+	Rect(int width, int height) { this->width = width; this->height = height; }
+	//다른 클래스인 RectManager class의 멤버 함수 equals()함수를 Rect class 의 프렌드 함수로 선언
+	friend bool RectManager::equals(Rect r, Rect s);
+};
+
+//RectManager::equals() 구현
+bool RectManager::equals(Rect r, Rect s) {
+	if (r.width == s.width && r.height == s.height)
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	Rect a(3, 4), b(3, 4);
+	RectManager man;
+
+	if (man.equals(a, b))
+		cout << "equal" << endl;
+	else
+		cout << "not equal" << endl;
+}
